779b24af5a700e39f7f7710a0c98394e04fc16a1
sunny <sunnyj0613@gmail.com>
2020-09-18 15:09:08 +0900

    Updates

---
+++ precache-manifest.a483d85d25aec4d245dcf7c564f1e02f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ecb27d0e3d665ad38b93f570d63c8d2",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "823e3b0e832b7977e2e2",
+    "url": "/movie_app_2020/static/css/main.03c7190c.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "823e3b0e832b7977e2e2",
+    "url": "/movie_app_2020/static/js/main.bc10d4aa.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
